b8cf99c28c2070ba9bfaa16eb5db5bc9322dea61
Takashi MATSUMURA <mats@coral.plala.or.jp>
2023-08-18 16:30:34 +0900

    responsive navbar

--- src/app/_components/common/Navbar.tsx
+++ src/app/_components/common/Navbar.tsx
@@ -4,15 +4,15 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header className="container mx-auto">
-      <div className="bg-blue-400 w-full fixed flex justify-around h-10 items-center">
-        <div>Care & Frail Support</div>
+    <header>
+      <div className="bg-gray-400 w-full fixed flex justify-around h-14 items-center">
+        <div className="text-2xl font-bold">Care & Frail Support</div>
         <div className="items-center space-x-3 hidden md:flex">
           <a href="#">Home</a>
           <a href="#">BBS</a>
           <a href="#">Calendar</a>
           <a href="#">
-            <button className="px-6 py-2 bg-theme rounded-lg hover:bg-purple-600 transition-all duration-300">
+            <button className="px-6 py-2 text-white bg-theme rounded-lg hover:bg-gray-800 tnpransition-all duration-300">
               logout
             </button>
           </a>
