bb52ec08d4e9291beed931f9ffd6fc6bef57fb67
Takashi MATSUMURA <mats@coral.plala.or.jp>
2023-08-24 19:16:04 +0900

    mod

---
+++ src/app/_components/support/VisitActions.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const VisitActions = () => {
+  return (
+    <div
+      className="h-1/2
+    "
+    >
+      <input type="checkbox" name="visit" id="visit" />
+      <label htmlFor="visit">訪問完了</label>
+    </div>
+  );
+};
+
+export default VisitActions;
